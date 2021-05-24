c6d38ae7aa7d0498fd5ff55efb76932d637e301e
Alexander G. Yang <spark8402@rogers.com>
2021-05-24 10:29:44 -0400

    learning events propagation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').innerHTML);
+document.querySelector('.message').innerHTML = 'Correct Number!'
+
+document.querySelector('.number').innerHTML = 13;
+document.querySelector('.score').innerHTML = 10;
+
+document.querySelector('.guess').value = 23; 
+console.log(document.querySelector('.guess').value);
+*/
+  
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (guess === 0) {
+    document.querySelector('.message').innerHTML = 'No Number!🛑';
+  }
+});
